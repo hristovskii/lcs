d11a3c548dfc7c73bab6534a6fd455ab0ad24213
Petar Hristovski <petarhristovski23@gmail.com>
2024-05-27 14:39:27 +0200

    adding iframe video

--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -8,8 +8,18 @@ import { RevealList } from  'next-reveal';
 export default function Sponsors() {
     return(
         <section className="md:mx-6 mx-auto lg:px-36 md:px-16 px-8 mt-12 mb-20">
-
+            
+            
         <RevealList interval={100} delay={800}>
+        <iframe width="auto" height="auto" 
+            className="w-full aspect-video self-stretch md:min-h-96"
+            src="https://www.youtube.com/embed/ybWgQzPXLwI?si=IEI_6VLlrDKFb1Oq" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; 
+            autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />
             <div className="lg:px-36 md:px-16 px-4 grid justify-center items-center">
                 {/* <Heading heading={"Logo"}></Heading> */}
                 <Image src={logo} alt="Logo" height={300} className="mt-8 mb-8"></Image>
